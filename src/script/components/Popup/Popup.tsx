2c21d8a76c7c082566016e96c090d0c4e8b3ae56
EvgeniaM6 <evgminukova6@gmail.com>
2024-02-21 19:29:34 +0200

    feat: add dark theme styles for popup

--- src/script/components/Popup/Popup.tsx
+++ src/script/components/Popup/Popup.tsx
@@ -7,17 +7,19 @@ function Popup({ children, close, confirm, confirmAction }: PopupProps) {
 
   return (
     <div className='overlay'>
-      <div className='popup'>
+      <div className={`popup ${theme}`}>
         <div className='popup__content'>
           <div className='popup__cross'>
-            <button className='popup__cross-btn' onClick={close}>
-              <span className='popup__cross-btn-img'></span>
+            <button className={`popup__cross-btn ${theme}`} onClick={close}>
+              <span className={`popup__cross-btn-img ${theme}`}></span>
             </button>
           </div>
           {children}
           <div className='popup__btns'>
             <button className={`popup__btns-btn btn-secondary ${theme}`} onClick={close}>No, Keep it</button>
-            <button className='popup__btns-btn btn-primary' onClick={confirm}>{`Yes, ${confirmAction} it`}</button>
+            <button className={`popup__btns-btn btn-primary ${theme}`} onClick={confirm}>
+              {`Yes, ${confirmAction} it`}
+            </button>
           </div>
         </div>
       </div>
