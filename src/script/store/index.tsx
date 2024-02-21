ab47b3d113e7cdaa5697ff3d1bb46f06bd9b465d
EvgeniaM6 <evgminukova6@gmail.com>
2024-02-21 12:47:03 +0200

    feat: create feeds request reducer

--- src/script/store/index.tsx
+++ src/script/store/index.tsx
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './authSlice';
 import { authApi } from '../redux/authApi';
 import { feedsApi } from '../redux/feedsApi';
+import feedsSlice from './feedsSlice';
 
 const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [feedsApi.reducerPath]: feedsApi.reducer,
     auth: authSlice,
+    feeds: feedsSlice,
   },
   middleware: (getDefaultMiddlware) =>
     getDefaultMiddlware().concat([authApi.middleware, feedsApi.middleware]),
