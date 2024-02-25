952956e6c3160669d1f2148c4237e28ddb134bd4
EvgeniaM6 <evgminukova6@gmail.com>
2024-02-25 14:07:37 +0200

    feat: show sent message and bot search when user send message

--- src/script/components/Chat/Chat.tsx
+++ src/script/components/Chat/Chat.tsx
@@ -1,5 +1,5 @@
 import ReactMarkdown from 'react-markdown';
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import { ThemeContext } from '../../../App';
 import { IMessageDTO } from '../../../public-common/interfaces/dto/message/imessage-dto';
@@ -11,11 +11,12 @@ import { SendMsgForm } from '../../models';
 function Chat({ messagesArr }: { messagesArr: IMessageDTO[] }) {
   const { theme } = useContext(ThemeContext);
   const chatsListRef = useRef<HTMLDivElement>(null);
+  const [sentContent, setSentContent] = useState('');
 
   const [chatId] = location.pathname.split('/').slice(2);
   const { accessToken } = getLocalStorageTokens();
 
-  const [sendMessage, { isSuccess }] = useSendMessageMutation();
+  const [sendMessage, { isSuccess, isLoading }] = useSendMessageMutation();
   const { refetch } = useGetMessagesByChatIdQuery({ accessToken, id: chatId });
 
   const {
@@ -25,9 +26,11 @@ function Chat({ messagesArr }: { messagesArr: IMessageDTO[] }) {
   } = useForm<SendMsgForm>();
 
   const handleSubmitForm = ({ content }: SendMsgForm) => {
+    setSentContent(content);
     const [chatId] = location.pathname.split('/').slice(2);
     const { accessToken } = getLocalStorageTokens();
     sendMessage({ accessToken, values: { chatId: Number(chatId), content }});
+    reset();
   };
 
   useEffect(() => {
@@ -35,12 +38,11 @@ function Chat({ messagesArr }: { messagesArr: IMessageDTO[] }) {
       top: chatsListRef.current.scrollHeight,
       behavior: 'smooth',
     });
-  }, [messagesArr]);
+  }, [messagesArr, sentContent]);
 
   useEffect(() => {
     if (isSuccess) {
       refetch();
-      reset();
     }
   }, [isSuccess]);
 
@@ -65,6 +67,24 @@ function Chat({ messagesArr }: { messagesArr: IMessageDTO[] }) {
             );
           })
         }
+        {isLoading && <>
+          <div className={`chat__message msg ${'user'} ${theme}`}>
+            <div className='msg__author'>
+              <div className={`msg__author-icon ${'user'} ${theme}`}></div>
+            </div>
+            <div className='msg__content'>
+              <ReactMarkdown>{sentContent}</ReactMarkdown>
+            </div>
+          </div>
+          <div className={`chat__message msg ${'bot'} ${theme}`}>
+            <div className='msg__author'>
+              <div className={`msg__author-icon ${'bot'} ${theme}`}></div>
+            </div>
+            <div className='msg__content'>
+              <ReactMarkdown>{'looking for answer...'}</ReactMarkdown>
+            </div>
+          </div>
+        </>}
       </div>
       <div className={`chat__input ${theme}`}>
         <form className='chat__form' onSubmit={handleSubmit(handleSubmitForm)}>
